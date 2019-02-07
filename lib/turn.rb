a099af49ccc0accdaec5a429cd11d8611fa21531
Oscar Frederick Perez Reyes <a01271794@itesm.mx>
2019-02-07 23:19:18 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -38,4 +38,9 @@ def turn(board)
   puts "Please enter 1-9:"
   user_input = gets.chomp
   index = input_to_index(user_input)
-end
+  if valid_move == true
+    move(board, index)
+    display_board(board)
+  else
+    return turn
+ end
