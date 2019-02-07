4d58c379dcf2f2ba20f6e7638cb4a884576b9b68
Oscar Frederick Perez Reyes <a01271794@itesm.mx>
2019-02-07 23:15:09 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -10,15 +10,28 @@ def input_to_index(user_input)
   return index
 end
 def valid_move?(board, index)
-  if index >= 0 && index <= 8
+  def position_taken?(array, location)
+    if array[location] == " " || array[location] == "" || array[location] == nil
+      return false
+    else
+      return true
+    end
+  end
+  def board_number?(number)
+    if number >= 0 && number <=8
+      return true
+    else
+      return false
+    end
+  end
+  if (position_taken?(board, index) == false && board_number?(index) == true)
     return true
   else
-    return false
+    return false    
   end
 end
 def turn(board)
   puts "Please enter 1-9:"
   user_input = gets.chomp
   index = input_to_index(user_input)
-
 end
